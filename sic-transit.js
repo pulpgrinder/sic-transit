5875a5ec748ccc1f2b41775c55a96865111cd872
Pulpgrinder Dev Team <pulpgrinder@protonmail.com>
2023-11-20 21:49:00 -0900

    More transitions.

--- sic-transit.js
+++ sic-transit.js
@@ -1,21 +1,97 @@
+/*  sic-transit.js Copyright 2023 by Anthony W. Hursh
+ *  MIT license
+ */
+
 class SicTransit {
-    dispatchTable =  {
-        "slideInLeft": {
-                        forwardTransition: this.slideInLeft,
-                        undoTransition: this.slideOutLeft,
-        },
-        "slideOutLeft": {
-            forwardTransition: this.slideOutLeft,
-            undoTransition: this.slideInLeft,
-}
-    }
     constructor(containerId, elementClass) {
       this.containerId = containerId;
+      this.container = document.querySelector(containerId);
       this.elementClass = elementClass;
       this.loadStack();
+      let overlay = document.createElement('div');
+      this.overlay = overlay;
+      overlay.className = 'sic-transit-overlay';
+      this.container.appendChild(overlay);
+      this.elementStack.unshift(overlay);
+      this.normalizeStack();
       this.showElement(this.elementStack[this.elementStack.length - 1]);     
     }
 
+    dispatchTable =  {
+        "fadeIn": {
+            forwardTransition: this.fadeIn,
+            animation: [{ display:"block", opacity: 0}, {display:"block",opacity:1}],
+            timing: {easing: 'ease-in-out', duration:500}
+        },
+        "fadeOut": {
+            forwardTransition: this.fadeOut,
+            animation: [{opacity:1}, {opacity:0}],
+            timing: {easing: 'ease-in-out', duration:500}
+        },
+        "slideInLeft": {
+                        forwardTransition: this.slideInLeft,
+                        animation: [{ display:"block", transform: "translateX(100%)"}, {display:"block",transform: "translateX(0%)"}],
+                        timing: {easing: 'ease-in-out', duration:500}
+        },
+        "slideOutLeft": {
+            forwardTransition: this.slideOutLeft,
+            animation: [{transform: "translateX(0%)"}, {transform: "translateX(100%)"}],
+            timing: {easing: 'ease-in-out', duration:500}
+        },
+        "slideInRight": {
+            forwardTransition: this.slideInRight,
+            animation: [{display:"block", transform: "translateX(-100%)"}, {display:"block",transform: "translateX(0%)"}],
+           timing: {easing: 'ease-in-out', duration:500}
+        },
+        "slideOutRight": {
+            forwardTransition: this.slideOutRight,
+            animation: [{transform: "translateX(0%)"}, {transform: "translateX(-100%)"}],
+            timing:{easing: 'ease-in-out',duration:500}
+        },
+        "slideInTop": {
+            forwardTransition: this.slideInTop,
+            animation: [{display:"block", transform: "translateY(-100%)"}, {display:"block",transform: "translateY(0%)"}],
+            timing: {easing: 'ease-in-out', duration:500}
+        },
+        "slideOutTop": {
+            forwardTransition: this.slideOutTop,
+            animation:[{transform: "translateY(0%)"}, {transform: "translateY(-100%)"}],
+            timing:{easing: 'ease-in-out', duration:500}
+        },
+        "slideInBottom": {
+            forwardTransition: this.slideInBottom,
+            animation: [{display:"block", transform: "translateY(100%)"}, {display:"block",transform: "translateY(0%)"}],
+            timing: {easing: 'ease-in-out', duration:500}
+        },
+        "slideOutBottom": {
+            forwardTransition: this.slideOutBottom,
+            animation: [{transform: "translateY(0%)"}, {transform: "translateY(100%)"}],
+            timing: {easing: 'ease-in-out', duration:500}
+        },
+        "newspaperIn": {
+            forwardTransition: this.newspaperIn,
+            animation: [{display:"block", transform: "rotate(0deg)  scale(0)"}, {display:"block", transform: "rotate(720deg) scale(1)"}],
+           timing: {easing: 'ease-in-out', duration:500}
+        },
+        "newspaperOut":{
+            forwardTransition: this.newspaperOut,
+            animation:[{display:"block", transform: "rotate(0deg)  scale(1)"}, {display:"block", transform: "rotate(-720deg) scale(0)"}],
+           timing: {easing: 'ease-in-out', duration:500}
+        },
+        "zoomIn": {
+            forwardTransition: this.zoomIn,
+            animation: [{display:"block", transform: "scale(0)"}, {display:"block", transform: "scale(1)"}],
+            timing: {easing: 'ease-in-out', duration:500}
+
+        },
+        "zoomOut":{
+            forwardTransition: this.zoomOut,
+            animation: [{display:"block", transform: "scale(1)"}, {display:"block", transform: "scale(0)"}],
+            timing: {easing: 'ease-in-out', duration:500}
+        }
+
+
+    }
     loadStack(){
         const element_list = document.querySelectorAll(this.elementClass);
         this.elementStack = [...element_list];
@@ -23,7 +99,8 @@ class SicTransit {
     }
     normalizeStack(){
         for(let index = 0; index < this.elementStack.length; index++){
-            this.elementStack[index].style.zIndex = index - this.elementStack.length;
+            let element = this.elementStack[index];
+            element.style.zIndex = index - this.elementStack.length;
         }
     }
     getZIndex(element){
@@ -40,9 +117,15 @@ class SicTransit {
         this.elementStack.push(element);
         this.normalizeStack();
     }
+    moveToBos(element){
+        this.removeFromStack(element);
+        this.elementStack.unshift(element);
+        this.normalizeStack();
+    }
     showElement(element){
         this.moveToTos(element);
         element.style.display = "block";
+        this.normalizeStack();
     }
     checkElementExists(selectedElement, elementSelector){
         if(selectedElement === null){
@@ -52,7 +135,6 @@ class SicTransit {
     selectElement(elementSelector){
         let selectedElement;
         if(Number.isInteger(elementSelector)){
-            console.log("got integer");
             // We're selecting the new item by index
             let elements = document.querySelectorAll(this.elementClass);
             selectedElement =  elements[elementSelector];
@@ -68,47 +150,165 @@ class SicTransit {
         if(transitionFunction === null){
             throw new Error("SicTransit: " + transitionName + " is not a recognized transition");
         }
-        transitionFunction(this,elementSelector,delay);
+        const animation = this.dispatchTable[transitionName]["animation"];
+        let timing = this.dispatchTable[transitionName]["timing"];
+        timing.duration = delay;
+        transitionFunction(this,elementSelector,animation,timing);
     }
-    slideInLeft(self,elementSelector,delay){
-        console.log("slideInLeft");
-        const selectedElement = self.selectElement(elementSelector);
-        console.log("newElement is " + selectedElement);
-        self.moveToTos(selectedElement);
-        const slideInLeftAnim = [{display:"block", transform: "translateX(100%)"}, {display:"block",transform: "translateX(0%)"}];
-        selectedElement.style.display = "block";
-        const slideInLeftTiming = {duration:delay,iterations:1};
-
-        selectedElement.animate(slideInLeftAnim,slideInLeftTiming);
-  /*      const aliceTumbling = [
-            { transform: "rotate(0) translate3D(-50%, -50%, 0)", color: "#000" },
-            { color: "#431236", offset: 0.3 },
-            { transform: "rotate(360deg) translate3D(-50%, -50%, 0)", color: "#000" },
-          ];
-          const aliceTiming = {
-            duration: 3000,
-            iterations: Infinity,
-          };
-          document.getElementById("alice").animate(aliceTumbling, aliceTiming); */
-       
-    }
-    slideOutLeft(self,elementSelector,delay){
-        console.log("slideInLeft");
-        const selectedElement = self.selectElement(elementSelector);
-        console.log("newElement is " + selectedElement);
-        self.knockdownElements();
-        self.currentElement.style.zIndex = 1;
-        selectedElement.style.zIndex = 2;
-        selectedElement.style.display = "block";
-        const slideOutLeftAnim = [{transform: "translateX(0%)"}, {transform: "translateX(100%)"}];
-        const slideOutLeftTiming = {duration:delay,iterations:1};
-        let slideOut = selectedElement.animate(slideOutLeftAnim,slideOutLeftTiming);
-        slideOut.onfinish = function(){
-            selectedElement.style.display = "none";
-            selectedElement.style.zIndex = -32;
-            selectedElement.style.transform = "translateX(0%)";
-            self.knockdownElements();
+    performAnimation(selectedElement,anim,direction,timing,onfinish){
+        this.moveToTos(selectedElement);
+        const animation = selectedElement.animate(anim,timing);
+        if(direction > 0){
+            this.overlay.style.opacity = 0.0;
+            this.overlay.animate({opacity:"0",opacity:"0.5"},timing);
         }
+        else{
+            this.overlay.style.opacity = 0.5;
+            this.overlay.animate({opacity:"0.5",opacity:"0"},timing);
+        }
+        animation.onfinish = onfinish;
+    }
+    slideInLeft(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            selectedElement.style.display= "block";
+            self.moveToBos(self.overlay);
+        }
+        self.performAnimation(selectedElement,animation,1,timing,finishHandler);
+    }
+    slideOutLeft(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(selectedElement);
+            self.moveToBos(self.overlay);
+            selectedElement.style.transform = "translateX(0%)";
+        }
+        self.performAnimation(selectedElement,animation,-1,timing,finishHandler);
+    }
+    slideInRight(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(self.overlay)
+            selectedElement.style.display= "block";
+        }
+        self.performAnimation(selectedElement,animation,1,timing,finishHandler);
+    }
+    slideOutRight(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(selectedElement);
+            self.moveToBos(self.overlay)
+            selectedElement.style.transform = "translateX(0%)";
+        }
+        self.performAnimation(selectedElement,animation,-1,timing,finishHandler);
+    }
+    slideInTop(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay)
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(self.overlay)
+            selectedElement.style.display= "block";
+        }
+        self.performAnimation(selectedElement,animation,1,timing,finishHandler);
+    }
+    slideOutTop(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay)
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(self.overlay)
+            self.moveToBos(selectedElement);
+            selectedElement.style.transform = "translateY(0%)";
+        }
+        self.performAnimation(selectedElement,animation,-1,timing,finishHandler);
+    }
+    slideInBottom(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay)
+        const selectedElement = self.selectElement(elementSelector);
+    
+        let finishHandler = function(){
+            self.moveToBos(self.overlay)
+            selectedElement.style.display= "block";
+        }
+        self.performAnimation(selectedElement,animation, 1,timing,finishHandler);
+    }
+    slideOutBottom(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
        
+        let finishHandler = function(){
+            self.moveToBos(self.overlay)
+            self.moveToBos(selectedElement);
+            selectedElement.style.transform = "translateY(0%)";
+        }
+        self.performAnimation(selectedElement,animation,-1,timing,finishHandler);
+    }
+    newspaperIn(self,elementSelector, animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);  
+        let finishHandler = function(){
+            self.moveToBos(self.overlay);
+            selectedElement.style.display = "block";
+            selectedElement.style.transform = "rotate(0deg) scale(1)";
+        }
+        self.performAnimation(selectedElement,animation,1,timing,finishHandler);
+    }
+    newspaperOut(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+      
+        let finishHandler = function(){
+            self.moveToBos(self.overlay);
+            self.moveToBos(selectedElement);
+            selectedElement.style.display = "none";
+            selectedElement.style.transform = "rotate(0deg) scale(1)";
+        }
+        self.performAnimation(selectedElement,animation,-1,timing,finishHandler);
+    }
+    zoomIn(self,elementSelector, animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(self.overlay);
+            selectedElement.style.display = "block";
+            selectedElement.style.transform = "scale(1)";
+        }
+        self.performAnimation(selectedElement,animation,1,timing,finishHandler);
+    }
+    zoomOut(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+      
+        let finishHandler = function(){
+            self.moveToBos(self.overlay);
+            self.moveToBos(selectedElement);
+            selectedElement.style.display = "none";
+            selectedElement.style.transform = "scale(1)";
+        }
+        self.performAnimation(selectedElement,animation,-1,timing,finishHandler);
+    }
+    fadeIn(self,elementSelector, animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(self.overlay);
+            selectedElement.style.display = "block";
+            selectedElement.style.opacity = 1;
+        }
+        self.performAnimation(selectedElement,animation,1,timing,finishHandler);
+    }
+    fadeOut(self,elementSelector,animation,timing){
+        self.moveToTos(self.overlay);
+        const selectedElement = self.selectElement(elementSelector);
+        let finishHandler = function(){
+            self.moveToBos(self.overlay);
+            self.moveToBos(selectedElement);
+            selectedElement.style.display = "none";
+            selectedElement.style.opacity = 1;
+        }
+        self.performAnimation(selectedElement,animation,-1,timing,finishHandler);
     }
   }
